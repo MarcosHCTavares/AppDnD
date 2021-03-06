30b845e1e7e83bb81b25d6f170844c4b5045430d
Marcos Tavares <72478803+MarcosHCTavares@users.noreply.github.com>
2021-03-06 19:54:45 -0300

    new

--- AppPy/Classes.py
+++ AppPy/Classes.py
@@ -1,65 +1,65 @@
 import random
-''#random nunbers v
+#random nunbers v
 modcon = 0
 nivel = 0
-''#random nunbers ^
-classe = str(input('Classe: '))
-if classe == 'Barbáro':
+#random nunbers ^
+classe = str(input('Classe: ')).upper().strip()
+if classe == 'BARBARO':
     if nivel == 1:
         vida = 12 + modcon
     else:
         vida = (random.randint(1, 12) + modcon) * nivel
-elif classe == 'Bardo':
+elif classe == 'BARDO':
     if nivel == 1:
         vida = 8 + modcon
     else:
         vida = (random.randint(1, 8) + modcon) * nivel
-elif classe == 'Bruxo':
+elif classe == 'BRUXO':
     if nivel == 1:
         vida = 8 + modcon
     else:
         vida = (random.randint(1, 8) + modcon) * nivel
-elif classe == 'Clérigo':
+elif classe == 'CLERIGO':
     if nivel == 1:
         vida = 8 + modcon
     else:
         vida = (random.randint(1, 8) + modcon) * nivel
-elif classe == 'Druida':
+elif classe == 'DRUIDA':
     if nivel == 1:
         vida = 8 + modcon
     else:
         vida = (random.randint(1, 8) + modcon) * nivel
-elif classe == 'Feiticeiro':
+elif classe == 'FEITICEIRO':
     if nivel == 1:
         vida = 6 + modcon
     else:
         vida = (random.randint(1, 6) + modcon) * nivel
-elif classe == 'Guerreiro':
+elif classe == 'GUERREIRO':
     if nivel == 1:
         vida = 10 + modcon
     else:
         vida = (random.randint(1, 10) + modcon) * nivel
-elif classe == 'Ladino':
+elif classe == 'LADINO':
     if nivel == 1:
         vida = 8 + modcon
     else:
         vida = (random.randint(1, 8) + modcon) * nivel
-elif classe == 'Mago':
+elif classe == 'MAGO':
     if nivel == 1:
         vida = 6 + modcon
     else:
         vida = (random.randint(1, 6) + modcon) * nivel
-elif classe == 'Monge':
+elif classe == 'MONGE':
     if nivel == 1:
         vida = 8 + modcon
     else:
         vida = (random.randint(1, 8) + modcon) * nivel
-elif classe == 'Paladino':
+elif classe == 'PALADINO':
     if nivel == 1:
         vida = 10 + modcon
     else:
         vida = (random.randint(1, 10) + modcon) * nivel
-elif classe == 'Patrulheiro':
+elif classe == 'PATRULHEIRO':
     if nivel == 1:
         vida = 10 + modcon
     else:
